bc30a7ceac409d9329612dfc2317aa7b6df9e6bb
maqsud <sunnatovmaqsud175@gmail.com>
2024-04-05 01:37:13 +0500

    first commit

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #extension
+    "import_export",
+
     #my apps
     'student',
     'library'
--- library/admin.py
+++ library/admin.py
@@ -1,7 +1,38 @@
 from django.contrib import admin
 from .models import Book, Author, BookingBook, Comments
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(BookingBook)
-admin.site.register(Comments)
+
+@admin.register(Author)
+class AuthorAdmin(ImportExportModelAdmin):
+    search_fields = ['name', 'country']
+
+@admin.register(Book)
+class BookAdmin(ImportExportModelAdmin):
+    list_display =("id", "title", "description", "price", "count", "author", "create_date")
+    list_display_links =("id", "title", "description", "price", "count", "author", "create_date")
+    search_fields = ['title', 'author__name']
+    ordering = ("title", "author")
+    autocomplete_fields = ("author", )
+
+@admin.register(BookingBook)
+class BookingBookAdmin(ImportExportModelAdmin):
+    list_display = ("id", "student", "book", "take_date", "returned_date")
+
+    def student(self):
+        return self.count()
+
+    def book(self):
+        return self.count()
+
+@admin.register(Comments)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("id", "text", "student")
+
+
+
+
+
+# admin.site.register(Author)
+# admin.site.register(BookingBook)
+# admin.site.register(Comments)
